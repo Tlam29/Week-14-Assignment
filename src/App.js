9ae429bb058d7b9dc69d2b00f59a64e6777304b9
Tyson Lam <Tysonlam21@gmail.com>
2022-11-03 02:50:42 -0500

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/jquery/dist/jquery'
+import MovieList from './components/MovieList'
+import MovieListHeading from './components/MovieListHeading';
+import Search from './components/Search';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('')
+  
+  const getMovie = async(searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=fa3f61f4`
+
+    const response = await fetch(url);
+    const responseJson = await response.json;
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  }
+
+    useEffect( () =>{
+      getMovie(searchValue);
+    }, [searchValue])
+  
+
+	 
+	return (
+		<div className='container-fluid'>
+			<div className='row'>
+        <MovieListHeading heading='Movies'/>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+      <div className='row'>
+        <MovieList movies = {movies} />
+      </div>
+		</div>
+	);
+};
 
 export default App;
